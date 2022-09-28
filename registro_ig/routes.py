db8564b5772d63b76fea351488d4048ba0c53d54
Your Name <you@example.com>
2022-09-28 12:37:58 +0200

    Nueva versión,creamos indice funcional enlazado con sqlite

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,6 +1,11 @@
-from operator import index
+from flask import render_template
 from registro_ig import app
+from registro_ig.models import select_all
 
 @app.route("/")
 def index():
-    return "Funcionando"
+    #Consultar todos los datos de la base de datos
+    registros = select_all()
+    return render_template("index.html", pageTitle ="Todos", data=registros)
+
+
